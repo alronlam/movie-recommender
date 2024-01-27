fa3a45397330abec3d6b8e9003e8fd697ec0b0f1
alron <alron.jan.lam@gmail.com>
2024-01-27 12:30:16 +0800

    Refactor: move SearchBar into own component

--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -7,7 +7,6 @@ const SearchBar = () => {
 
     return (
         <div>
-            <h1 className="text-xl">Hello World!</h1>
             <header className="sticky top-0 bg-neongreen p-4 flex items-center justify-center h-[10vh] w-screen">
 
                 <form onSubmit={handleSubmit(onSubmit)} action="/search" method="GET" className='w-full flex items-center justify-center'>
@@ -16,7 +15,7 @@ const SearchBar = () => {
                         className="w-4/5 md:w-3/5 lg:w-2/5  px-4 py-2 border rounded"
                         placeholder="Describe the kind of movie you like..."
                         type="search"
-                        {...register("q")}
+                        {...register("query")}
                     />
                 </form>
             </header>
