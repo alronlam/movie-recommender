0c75b1123aa7fa59c6332f906762c6496706955f
alron <alron.jan.lam@gmail.com>
2024-01-27 12:32:55 +0800

    Refactor: move css into styles folder

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import SearchBar from './components/SearchBar';
-import "./index.css";
+import "./styles/tailwind.css";
 
 
 const page = (
