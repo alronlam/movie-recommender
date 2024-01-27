f13d2690510904f40c92b5ed54d980c24ffb5edf
alron <alron.jan.lam@gmail.com>
2024-01-27 15:49:16 +0800

    Add more styling to MovieCard

--- js/components/MovieCard.js
+++ js/components/MovieCard.js
@@ -2,24 +2,33 @@
 
 import React, { useState } from 'react';
 
-const MovieCard = ({ title, overview, genres }) => {
+const MovieCard = ({ title, overview, genres, year, imdbUrl, imageUrl }) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
     return (
-        <div className="flex justify-center mt-5 mb-5">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-[80vw] lg:w-[40vw]  border border-black">
-                <h1 className="text-2xl font-bold mb-4">{title}</h1>
-                <p className="text-gray-600 mb-4">{overview}</p>
-                <div className="flex flex-wrap">
-                    {genres.map((genre, index) => (
-                        <span key={index} className="bg-blue-500 text-white rounded-full px-2 py-1 mr-2 mb-2 border">
-                            {genre}
-                        </span>
-                    ))}
+        <div className="flex flex-col lg:flex-row justify-center items-center mt-5 mb-5 ">
+            <div className="flex bg-white rounded-lg shadow-lg p-6 w-[80vw] border border-black">
+                {/* Movie Poster */}
+                <img className="h-64 mr-5 self-center" src={imageUrl} alt="Movie Poster" />
+
+                {/* Text Content */}
+                <div>
+                    <a className="text-2xl font-bold mb-10 underline" target="_blank" href={imdbUrl}>
+                        {title} ({year})
+                    </a>
+                    <p className="text-gray-600 mt-5 mb-4">{overview}</p>
+                    <div className="flex flex-wrap">
+                        {genres.map((genre, index) => (
+                            <span key={index} className="bg-blue-500 text-white rounded-full px-2 py-1 mr-2 mb-2 border">
+                                {genre}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -7,12 +7,12 @@ const SearchBar = () => {
 
     return (
         <div>
-            <header className="sticky top-0 bg-neongreen p-4 flex items-center justify-center h-[10vh] w-screen">
 
+            <header className="sticky top-0 bg-slate-600 p-4 flex items-center justify-center h-[10vh] w-screen">
                 <form onSubmit={handleSubmit(onSubmit)} action="/search" method="GET" className='w-full flex items-center justify-center'>
                     <input
                         autoFocus
-                        className="w-4/5 md:w-3/5 lg:w-2/5  px-4 py-2 border rounded"
+                        className="w-full md:w-3/5  px-4 py-2 border rounded"
                         placeholder="Describe the kind of movie you like..."
                         type="search"
                         {...register("query")}
