007ee77d1f9252b84dd19c6ca6eb139681d5275d
alron <alron.jan.lam@gmail.com>
2024-02-08 10:55:09 +0800

    Add Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,18 @@ FROM python:3.10-slim-bullseye
 
 WORKDIR /app
 
-COPY requirements.txt /app/
-RUN pip install --upgrade && pip install -r requirements.txt
+COPY requirements.txt .
+RUN pip install pip-tools==7.3.*
+RUN pip install -r requirements.txt
 
-COPY . /app
+# RUN apt-get update && apt-get -y install npm@9.5.1
+# COPY package*.json .
+# RUN npm install
+# RUN npm run prod
 
-RUN python manage.py collectstatic --noinput && python manage.py migrate
-RUN npm run prod
+COPY . .
+RUN python manage.py migrate
+RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
-CMD ["gunicorn", "config:wsgi"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "config.wsgi:application"]
--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,8 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
